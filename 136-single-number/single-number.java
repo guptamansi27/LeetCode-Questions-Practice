7e84d49028204462702d45040542d88ca8768b24
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-05-14 22:22:48 +0530

    Time: 1 ms (99.96%) | Memory: 47 MB (28.07%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n=nums.length;
+       /* M1- Brute force ,t.c-O(n^2)
+       int n=nums.length;
         for(int i=0;i<n;i++){
             int count=0;
             for(int j=0;j<n;j++){
@@ -10,6 +11,13 @@ class Solution {
             if(count==1)
                 return nums[i];
         }
-        return -1;
+        return -1;*/
+        //Method 2 t.c-O(n)
+
+        int ans=0;
+        for(int num:nums){
+             ans=ans^num;
+        }
+        return ans;
     }
 }
