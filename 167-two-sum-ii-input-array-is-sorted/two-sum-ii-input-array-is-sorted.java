d57b9d721d18169d93479ab20bdcdeb771139bf0
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-05-20 23:49:15 +0530

    Time: 2 ms (96.37%) | Memory: 48.3 MB (78.19%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] n, int target) {
+        /*Method 1 ... TC-O(n^2)
         int num = n.length;
         int[] a = new int[2];
 
@@ -8,13 +9,35 @@ class Solution {
             for(int j = i + 1; j < num; j++) {
 
                 if(n[i] + n[j] == target) {
-                    a[0] = i+1;
+                    a[0] = i+1; //as indices in problem starts with 1 not 0
                     a[1] = j+1;
                     return a;
                 }
             }
         }
 
-        return a;
+        return a;*/
+        //Method 2 
+         int left = 0;
+        int right = n.length - 1;
+
+        while(left < right) {
+
+            int sum = n[left] + n[right];
+
+            if(sum == target) {
+                return new int[]{left + 1, right + 1};
+            }
+
+            else if(sum < target) {
+                left++;
+            }
+
+            else {
+                right--;
+            }
+        }
+
+        return new int[]{};
     }
 }
