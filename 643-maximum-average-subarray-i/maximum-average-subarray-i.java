aaa808576e1b1ffcc0f59be56a355582df645b01
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-06-03 13:07:34 +0530

    Time: 3 ms (79.65%) | Memory: 69.5 MB (76.81%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        /*Brute force */
+        /*Brute force  tc-O(n*k)
         int n = nums.length;
         int maxSum = Integer.MIN_VALUE;
         //Iterate through all possible subarrays of length k
@@ -13,6 +13,27 @@ class Solution {
             //update maxSum
             maxSum=Math.max(sum,maxSum);
         }
-        return (double)maxSum/k;
+        return (double)maxSum/k; */
+        
+        /*OPtimal Approach */
+          int sum = 0;
+
+        // Sum of first window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+
+        // Slide the window
+        for (int i = k; i < nums.length; i++) {
+
+            sum -= nums[i - k];   // remove old element
+            sum += nums[i];       // add new element
+
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return (double) maxSum / k;
     }
 }
