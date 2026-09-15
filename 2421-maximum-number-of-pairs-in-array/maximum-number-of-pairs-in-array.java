626bf8d09b8ac670736cc6b2caf657112bc3acef
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-09-16 01:09:30 +0530

    Time: 2 ms (55.05%) | Memory: 43.7 MB (45.54%) - LeetSync

--- 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] numberOfPairs(int[] nums) {
-        int ans[]= new int[2];
+        //int ans[]= new int[2];
         int p=0,r=0;
         HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int n : nums){
             map.put(n,map.getOrDefault(n,0)+1);
         }
-
+        /*
         for(Integer v : map.values()){
             if(v%2==0){
                 p+=(v/2);
@@ -19,7 +19,13 @@ class Solution {
         ans[0]=p;
         ans[1]=r;
 
-        return ans;
+        return ans;*/
 
+        //Method 2
+        for(Integer v : map.values()){
+            p+=(v/2);
+            r+=(v%2);
+         }
+         return new int[]{p,r};
     }
 }
