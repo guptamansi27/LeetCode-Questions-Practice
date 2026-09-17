e02fb31aed4034bd6a87f823864a9852586cc219
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-09-17 18:32:29 +0530

    Time: 65 ms (43.36%) | Memory: 48.1 MB (10.67%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -12,7 +12,8 @@ class Solution {
                 left++;
             }
             seen.add(s.charAt(right));
-            maxLength = Math.max(maxLength,right-left+1);
+            //maxLength = Math.max(maxLength,right-left+1);
+            maxLength = Math.max(maxLength,seen.size());
        }
        return maxLength;
     }
